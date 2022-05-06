1ca0bf3ce870a3455f4cdfaf2df7ba1a20983696
Juliette Cordor <grassojames5@gmail.com>
2022-05-06 13:19:52 +1000

    fixes

--- build.rs
+++ build.rs
@@ -4,5 +4,8 @@ fn main() {
     Build::new()
         .cpp(true)
         .file("cppspin/include/spinners.hpp")
+        .flag_if_supported("-Werror")
+        .flag_if_supported("-Wall")
+        .flag_if_supported("-pedantic")
         .compile("spinners")
 }
