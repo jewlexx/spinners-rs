706179640379535e4a448cc7d2225bb6666cde01
Juliette Cordor <grassojames5@gmail.com>
2022-05-06 14:19:46 +1000

    fixes

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use spinners::Spinner;
 mod spinners;
 
 pub fn spin(spinner: &Spinner) {
-    let spinner = *spinner;
+    let spinner = spinner.clone();
     let interval = spinner.interval;
     let frames = spinner.frames;
 }
