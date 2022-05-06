01697644e949d90bc03f346b3063fdeecbe39656
Juliette Cordor <grassojames5@gmail.com>
2022-05-06 20:43:13 +1000

    added docs

--- src/spinner.rs
+++ src/spinner.rs
@@ -39,6 +39,27 @@ impl Spinners {
 }
 
 impl Spinner {
+    /// Create a new spinner along with a message
+    ///
+    /// # Examples
+    ///
+    /// Basic Usage:
+    ///
+    /// ```
+    /// use spinners_rs::{Spinner, Spinners};
+    ///
+    /// let sp = Spinner::new(Spinners::Dots, "Doing some cool things...").unwrap();
+    /// sp.start();
+    /// ```
+    ///
+    /// No Message:
+    ///
+    /// ```
+    /// use spinners_rs::Spinners;
+    ///
+    /// let sp = Spinners::Dots.into_spinner().unwrap();
+    /// sp.start();
+    /// ```
     pub fn new<S: Into<String>>(spinner: Spinners, message: S) -> Result<Self, Error> {
         let frames = spinner.get_frames();
 
@@ -52,22 +73,6 @@ impl Spinner {
             })
         } else {
             Err(Error::UnknownSpinner(spinner.to_string()))
-        }
-    }
-
-    pub fn set_interval(&mut self, interval: u64) {
-        self.interval = interval;
-        if let Some(sender) = &self.sender {
-            sender.send(Event::SetInterval(interval)).unwrap();
-        }
-    }
-
-    pub fn set_message<S: Into<String>>(&mut self, message: S) {
-        self.message = message.into();
-        if let Some(sender) = &self.sender {
-            sender
-                .send(Event::SetMessage(self.message.clone()))
-                .unwrap();
         }
     }
 
@@ -100,9 +105,82 @@ impl Spinner {
         self.sender = Some(sender);
     }
 
+    /// Stops the spinner from running
+    ///
+    /// Basic Usage:
+    ///
+    /// ```
+    /// use spinners_rs::Spinners;
+    ///
+    /// let sp = Spinners::Dots.into_spinner().unwrap();
+    /// sp.start();
+    ///
+    /// thread::sleep(Duration::from_millis(1000));
+    ///
+    /// sp.stop();
+    /// ```
     pub fn stop(&self) {
         if let Some(sender) = &self.sender {
             sender.send(Event::Stop).unwrap();
+        }
+    }
+
+    /// Updates the frame interval
+    ///
+    /// This changes how fast each frame comes up
+    /// This can be changed before the spinner is started or after
+    ///
+    /// Basic Usage:
+    ///
+    /// ```
+    /// use spinners_rs::Spinners;
+    ///
+    /// let sp = Spinners::Dots.into_spinner().unwrap();
+    /// sp.start();
+    ///
+    /// // Will run through one iteration of frames
+    /// thread::sleep(Duration::from_millis(1000));
+    ///
+    /// sp.set_interval(500);
+    ///
+    /// // Will now run through two iterations of the frames
+    /// thread::sleep(Duration::from_mills(1000));
+    ///
+    /// sp.stop();
+    /// ```
+    pub fn set_interval(&mut self, interval: u64) {
+        self.interval = interval;
+        if let Some(sender) = &self.sender {
+            sender.send(Event::SetInterval(interval)).unwrap();
+        }
+    }
+
+    /// Sets the message to display
+    ///
+    /// Similar to [`Spinner::set_interval`], this can be set before or after a spinner is started
+    ///
+    /// Basic Usage:
+    ///
+    /// ```
+    /// use spinners_rs::Spinners;
+    ///
+    /// let sp = Spinners::Dots.into_spinner().unwrap();
+    /// sp.start();
+    ///
+    /// thread::sleep(Duration::from_millis(1000));
+    ///
+    /// sp.set_message("Doing some cool things...");
+    ///
+    /// thread::sleep(Duration::from_mills(1000));
+    ///
+    /// sp.stop();
+    /// ```
+    pub fn set_message<S: Into<String>>(&mut self, message: S) {
+        self.message = message.into();
+        if let Some(sender) = &self.sender {
+            sender
+                .send(Event::SetMessage(self.message.clone()))
+                .unwrap();
         }
     }
 }
