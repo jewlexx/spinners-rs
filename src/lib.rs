d3a0c493fc7ee1d2eafbec466a37225641c602a1
Juliette Cordor <grassojames5@gmail.com>
2022-05-06 17:45:17 +1000

    fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
-use std::{
-    io::{stdout, Write},
-    sync::mpsc::{channel, TryRecvError},
-    thread,
-    time::Duration,
-};
-
-use spinners::Spinner;
-
 mod spinners;
 
+pub use spinners::*;
