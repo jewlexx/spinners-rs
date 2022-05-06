3b813e75e70c54c9323f03e3027b951feaac6711
Juliette Cordor <grassojames5@gmail.com>
2022-05-06 14:04:58 +1000

    fixes

--- src/spinners.rs
+++ src/spinners.rs
@@ -1,22 +1,56 @@
-extern "C" {
-    struct Spinner {
-    }
+use std::collections::HashMap;
 
-    impl Spinner {
-        pub fn setInterval(self, interval: cty::c_int);
-        pub fn setText(self, text: *const cty::c_char);
-        pub fn setSymbols(self, symbols: *const cty::c_char);
+use lazy_static::lazy_static;
+use maplit::hashmap;
 
-        pub fn startSpinner(self);
-        pub fn start(self);
-        pub fn stop(self);
-
-        pub fn new() -> Self;
-    }
-}
-
-fn spin() {
-    unsafe {
-        let sp = Spinner::new();
-    }
+lazy_static! {
+    static ref SPINNER_MAP: HashMap<&'static str, &'static str> = {
+        hashmap! {
+        "dots" => "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏",
+        "dots2" => "⣾⣽⣻⢿⡿⣟⣯⣷",
+        "dots3" => "⠋⠙⠚⠞⠖⠦⠴⠲⠳⠓",
+        "dots4" => "⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆",
+        "dots5" => "⠋⠙⠚⠒⠂⠂⠒⠲⠴⠦⠖⠒⠐⠐⠒⠓⠋",
+        "dots6" => "⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠴⠲⠒⠂⠂⠒⠚⠙⠉⠁",
+        "dots7" => "⠈⠉⠋⠓⠒⠐⠐⠒⠖⠦⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈",
+        "dots8" => "⠁⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈⠈",
+        "dots9" => "⢹⢺⢼⣸⣇⡧⡗⡏",
+        "dots10" => "⢄⢂⢁⡁⡈⡐⡠",
+        "dots11" => "⠁⠂⠄⡀⢀⠠⠐⠈",
+        "pipe" => "┤┘┴└├┌┬┐",
+        "star" => "✶✸✹✺✹✷",
+        "star2" => "+x*",
+        "flip" => "___-``'´-___",
+        "hamburger" => "☱☲☴",
+        "growVertical" => "▁▃▄▅▆▇▆▅▄▃",
+        "growHorizontal" => "▏▎▍▌▋▊▉▊▋▌▍▎",
+        "balloon" => " .oO@* ",
+        "balloon2" => ".oO°Oo.",
+        "noise" => "▓▒░",
+        "bounce" => "⠁⠂⠄⠂",
+        "boxBounce" => "▖▘▝▗",
+        "boxBounce2" => "▌▀▐▄",
+        "triangle" => "◢◣◤◥",
+        "arc" => "◜◠◝◞◡◟",
+        "circle" => "◡⊙◠",
+        "squareCorners" => "◰◳◲◱",
+        "circleQuarters" => "◴◷◶◵",
+        "circleHalves" => "◐◓◑◒",
+        "squish" => "╫╪",
+        "toggle" => "⊶⊷",
+        "toggle2" => "▫▪",
+        "toggle3" => "□■",
+        "toggle4" => "■□▪▫",
+        "toggle5" => "▮▯",
+        "toggle6" => "ဝ၀",
+        "toggle7" => "⦾⦿",
+        "toggle8" => "◍◌",
+        "toggle9" => "◉◎",
+        "toggle10" => "㊂㊀㊁",
+        "toggle11" => "⧇⧆",
+        "toggle12" => "☗☖",
+        "toggle13" => "=*-",
+        "arrow" => "←↖↑↗→↘↓↙"
+        }
+    };
 }
