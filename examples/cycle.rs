415024222d63d865be5694f573fa09bda09df628
Juliette Cordor <grassojames5@gmail.com>
2022-05-06 20:47:31 +1000

    fixed example

--- examples/cycle.rs
+++ examples/cycle.rs
@@ -7,10 +7,10 @@ use strum::IntoEnumIterator;
 fn main() {
     let sps = Spinners::iter().collect::<Vec<Spinners>>();
     let len = sps.len();
-    let mut sp = sps.get(0).unwrap().into_spinner().unwrap();
-    sp.start();
     for i in 0..sps.len() {
-        sp.set_message(format!("{}/{}", i, len));
+        let mut sp = sps.get(i).unwrap().into_spinner().unwrap();
+        sp.set_message(format!("{} {}/{}", sp.spinner, i, len));
+        sp.start();
 
         thread::sleep(Duration::from_millis(1000));
     }
