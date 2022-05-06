50d139115c86f2bd135ab6cf96092918f8d00a2a
Juliette Cordor <grassojames5@gmail.com>
2022-05-06 13:17:33 +1000

    fixes

--- src/spinners.rs
+++ src/spinners.rs
@@ -6,6 +6,11 @@ extern "C" {
         pub fn setInterval(self, interval: cty::c_int);
         pub fn setText(self, text: *const cty::c_char);
         pub fn setSymbols(self, symbols: *const cty::c_char);
+
+        pub fn startSpinner(self);
+        pub fn start(self);
+        pub fn stop(self);
+    
     }
 }
 
