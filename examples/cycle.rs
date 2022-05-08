9aa409ac1997f836c890a611030b05077ebdefff
Juliette Cordor <grassojames5@gmail.com>
2022-05-08 12:10:46 +1000

    fixed message formatting, formatted spinner list, and other fixes.

--- examples/cycle.rs
+++ examples/cycle.rs
@@ -4,14 +4,23 @@ use spinners_rs::Spinners;
 
 use strum::IntoEnumIterator;
 
+// This exists so that shorter names work properly with the formatted output.
+const WHITESPACE: &str = "                                                  ";
+
 fn main() {
     let sps = Spinners::iter().collect::<Vec<Spinners>>();
     let len = sps.len();
     for i in 0..sps.len() {
         let mut sp = sps.get(i).unwrap().into_spinner().unwrap();
-        sp.set_message(format!("{:<30} {}/{}", sp.spinner, i + 1, len));
+        sp.set_message(format!(
+            " {:0>2}/{} {}{}",
+            i + 1,
+            len + 1,
+            sp.spinner,
+            WHITESPACE
+        ));
         sp.start();
 
-        thread::sleep(Duration::from_millis(1000));
+        thread::sleep(Duration::from_millis(100));
     }
 }
